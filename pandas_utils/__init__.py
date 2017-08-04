9724e900e097bb7a27d798eeb4a1537a98cb613d
Shiutang-Li <stazlee@hotmail.com>
2017-08-04 02:11:13 -0600

    revision

--- pandas_utils/__init__.py
+++ pandas_utils/__init__.py
@@ -1,6 +1,6 @@
 # imports
 
 from pandas_utils.add_features              import add_features
-from pandas_utils.missing_values_count      import missing_values_count 
+from pandas_utils.structure                 import structure
 from pandas_utils.repeated_columns_detector import repeated_columns_detector
 from pandas_utils.update_data               import update_data
