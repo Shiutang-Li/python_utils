53cf6c2885c639607ebff2d1e9799cc598797781
Shiutang-Li <Shiutang-Li@users.noreply.github.com>
2018-03-19 22:19:45 -0600

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 # imports
 
-from pandas_utils.add_features              import add_features
-from pandas_utils.structure                 import structure
-from pandas_utils.repeated_columns_detector import repeated_columns_detector
-from pandas_utils.update_data               import update_data
+from python_utils.gpby                      import gp_by
+from python_utils.structure                 import structure
+from python_utils.repeated_columns_detector import repeated_columns_detector
+from python_utils.update_data               import update_data
