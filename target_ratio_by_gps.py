be1c9c7dca0335e3220c58b451c1aa4ba35fe712
Shiutang-Li <Shiutang-Li@users.noreply.github.com>
2018-03-20 09:44:06 -0600

    Create target_ratio_by_gps.py

---
+++ target_ratio_by_gps.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+# coding=utf8
+# description     : calculate terget column ratio
+# author          : Shiu-Tang Li
+# last update     : 03/19/2018
+# python_version  : 3.5.2
+
+import pandas as pd
+import numpy as np
+    
+def division(a, b, precision):
+    
+    
+def target_ratio_by_gps(df, target_column, target_value, groups, precision):
+    
+    # Input:
+    # df:                target dataframe
+    # target column:
+    # groups:
+
+    
+    # Output:            
+    
+    # Example:
+ 
+    
