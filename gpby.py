52315f1fd65688e957ce3e96dee3913fa1774ee2
Shiutang-Li <Shiutang-Li@users.noreply.github.com>
2018-03-26 12:37:37 -0600

    Update gpby.py

--- gpby.py
+++ gpby.py
@@ -25,27 +25,32 @@ def gpby(df, gp_feature, aggr_feature, aggr_func, join = True):
     # df     =  pd.DataFrame({'a':[1,1,2,2,3,3], 'b': [4,5,6,7,8,9]})
     # new_df =  gpby(df, 'a', 'b', 'mean')  
     
-    groups = df[[gp_feature, aggr_feature]].groupby(gp_feature)
     if aggr_func == 'mean':
-        table  = groups.aggregate(np.mean)[aggr_feature]
+        table  = df[[gp_feature, aggr_feature]].groupby(gp_feature).aggregate(np.mean)[aggr_feature]
     elif aggr_func == 'count':
-        table  = groups.aggregate('count')[aggr_feature]
+        table  = df[[gp_feature, aggr_feature]].groupby(gp_feature).aggregate('count')[aggr_feature]
     elif aggr_func == 'max':
-        table  = groups.aggregate(np.max)[aggr_feature]        
+        table  = df[[gp_feature, aggr_feature]].groupby(gp_feature).aggregate(np.max)[aggr_feature]        
     elif aggr_func == 'min':
-        table  = groups.aggregate(np.min)[aggr_feature]          
+        table  = df[[gp_feature, aggr_feature]].groupby(gp_feature).aggregate(np.min)[aggr_feature]          
     elif aggr_func == 'sum':
-        table  = groups.aggregate(np.sum)[aggr_feature]  
+        table  = df[[gp_feature, aggr_feature]].groupby(gp_feature).aggregate(np.sum)[aggr_feature]  
     elif aggr_func == 'list':
-        table  = groups.aggregate('list')[aggr_feature]   
+        table  = df[[gp_feature, aggr_feature]].drop_duplicates().groupby(gp_feature)[aggr_feature].apply(list) 
     else:
         return 'aggr function not available'
         
         
     table  = pd.DataFrame(table)
     table[gp_feature] = table.index
-    table  = pd.DataFrame({gp_feature:table[gp_feature].values,
-                           aggr_func +'_of_'+ aggr_feature +'_gp_by_'+ gp_feature: table[aggr_feature].values})
+    
+    if aggr_func != 'list':
+        table  = pd.DataFrame({gp_feature:table[gp_feature].values,
+                               aggr_func +'_of_'+ aggr_feature +'_gp_by_'+ gp_feature: table[aggr_feature].values})
+    else:
+        table  = pd.DataFrame({gp_feature:table[gp_feature].values,
+                               aggr_func +'_of_'+ aggr_feature +'_gp_by_'+ gp_feature: 
+                               [', '.join(str(e) for e in item) for item in table[aggr_feature].values]})
         
     if join == True:
         return df.join(table.set_index(gp_feature), on=gp_feature)
