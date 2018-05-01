e6e9dd978e62baf55b53b65bf04b24b2129319b9
Shiutang-Li <Shiutang-Li@users.noreply.github.com>
2018-05-01 15:28:57 -0600

    Create value_stats.py

---
+++ value_stats.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+# description     : enhanced value_counts(), including cumulated sum and percentage in the output dataframe
+# author          : Shiu-Tang Li
+# last update     : 05/01/2018
+
+def value_stats(df, column, order='asc', precision=3):
+   
+   num_records = df.shape[0]
+   table = pd.DataFrame(df[column].value_counts())
+   table.columns = ['cnt']
+   table['value'] = table.index
+   
+   if order == 'asc':
+       table.sort_values(by = 'value', inplace = True)
+   elif order == 'desc':
+       table.sort_values(by = 'value', ascending = False, inplace = True)
+   else:
+       print('argument error for "order"')
+       return None
+   
+   table['cum_cnt'] = table['cnt'].cumsum()
+   table['percentage'] = table.apply(
+       lambda x: round(x['cnt']*100.0 / num_records, precision), axis=1)
+   table['cum_percentage'] = table.apply(
+       lambda x: round(x['cum_cnt']*100.0 / num_records, precision), axis=1)
+   table.reset_index(inplace = True)
+   
+   return table[['value', 'cnt',  'cum_cnt', 'percentage', 'cum_percentage']]
+   
