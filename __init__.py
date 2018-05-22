a6b5cd8483d48d34408abe767ee6122319e98bbb
Shiutang-Li <Shiutang-Li@users.noreply.github.com>
2018-05-22 14:27:09 -0600

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,4 +4,3 @@ from python_utils.gpby                      import gpby
 from python_utils.structure                 import structure
 from python_utils.repeated_columns          import repeated_columns
 from python_utils.update_data               import update_data
-from python_utils.target_ratio_by_gps       import target_ratio_by_gps
