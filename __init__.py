4717b307c468c93796e41dd70ad9d1f490b6cccd
Shiutang-Li <Shiutang-Li@users.noreply.github.com>
2018-05-22 16:42:08 -0600

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 # imports
 
-from python_utils.gpby                      import gpby
+from python_utils.gpby                      import gpby, position_to_range, gpby_range
 from python_utils.structure                 import structure
 from python_utils.repeated_columns          import repeated_columns
 from python_utils.update_data               import update_data
