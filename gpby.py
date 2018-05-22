91a79683cf24a08ac6f075977fff0278dd7da6a2
Shiutang-Li <Shiutang-Li@users.noreply.github.com>
2018-05-22 13:00:46 -0600

    Update gpby.py

--- gpby.py
+++ gpby.py
@@ -47,13 +47,36 @@ def gpby(df, gp_feature, aggr_feature, aggr_func, join = True):
     
     if aggr_func != 'list':
         table  = pd.DataFrame({gp_feature:table[gp_feature].values,
-                               aggr_func +'_of_'+ aggr_feature +'_gp_by_'+ gp_feature: table[aggr_feature].values})
+                               aggr_func +'_of_'+ aggr_feature: table[aggr_feature].values})
     else:
         table  = pd.DataFrame({gp_feature:table[gp_feature].values,
-                               aggr_func +'_of_'+ aggr_feature +'_gp_by_'+ gp_feature: 
+                               aggr_func +'_of_'+ aggr_feature: 
                                [', '.join(str(e) for e in item) for item in table[aggr_feature].values]})
         
     if join == True:
         return df.join(table.set_index(gp_feature), on=gp_feature)
     else:
         return table
+
+
+def classify(x, value_list):
+    
+    for i in range(len(value_list)):
+        if value_list[i] <= x < value_list[i+1]:
+            return str(value_list[i])+' ≤ x < '+ str(value_list[i+1])
+    
+    
+def gpby_range(df, gp_feature, aggr_feature, aggr_func, value_list):
+    
+    # Input:
+    # df:                target dataframe
+    # gp_feature:        group by this feature
+    # aggr_feature:      another feature for aggregation
+    # aggr_func:         aggregation function: 'unique_count', 'count', 'mean', 'max', 'min', 'sum', 'list'
+    # value_list:        group by range is based on the value list. Needs to be in ascending order.
+    
+    # Output:            new dataframe
+    
+    df[gp_feature + ': x'] = df.apply(lambda x: classify(x[gp_feature], value_list), axis = 1)
+    return gpby(df, gp_feature + ': x', aggr_feature, aggr_func, join = False)
+    
