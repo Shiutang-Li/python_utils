a015424c43a4b65955872d77ef79982aac361675
Shiutang-Li <Shiutang-Li@users.noreply.github.com>
2018-05-22 23:06:07 -0600

    Update update_data.py

--- update_data.py
+++ update_data.py
@@ -3,7 +3,7 @@
 
 # description     : Update dataframe values using another dataframe 
 # author          : Shiu-Tang Li
-# last update     : 06/13/2017
+# last update     : 5/22/2018
 # python_version  : 3.5.2
 
 import pandas as pd
@@ -15,16 +15,11 @@ def update_data(df1, df2, key_column, sort = True):
     # Input:
     # df1:        target dataframe to be updated
     # df2:        used to update df1. Make sure all columns in df2 are in df1.
-    # key_column: the primary key used to match df1 and df2
+    # key_column: the column used to match df1 and df2
     # sort:       True:  output sorted revised dataframe by key_column  
     #             False: output revised dataframe, not sorted
     
     # Output:     updated dataframe
-    
-    # Example:
-    # df1 =  pd.DataFrame({'a':[1,2,3], 'b': [4,5,6], 'c': [7,8,9]})
-    # df2 =  pd.DataFrame({'a':[3,1],   'b': [9,2]})
-    # updated_df = update_data(df1, df2, 'a', sort = True)
     
     match_index = df1.apply(lambda x: x[key_column] in df2[key_column].values, axis=1)
     matched     = deepcopy(df1[match_index])
