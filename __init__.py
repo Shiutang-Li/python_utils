084fd7a4c6718332e41c64e2bbc0f871067fdb0f
Shiutang-Li <Shiutang-Li@users.noreply.github.com>
2018-05-23 14:41:06 -0600

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,4 +4,4 @@ from python_utils.gpby                      import gpby, position_to_range, gpby
 from python_utils.structure                 import structure
 from python_utils.duplicate_columns         import duplicate_columns
 from python_utils.update_data               import update_data
-from python_utils.val_cnt                   import val_cnt
+from python_utils.val_cnt                   import val_cnt, position_to_range_val_cnt, val_cnt_range
